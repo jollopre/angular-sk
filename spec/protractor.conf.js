baa5c10d3f3d732dc71d39520ca0e3ba79fdee9c
Jose Lloret <jose.lloret@ed.ac.uk>
2015-10-20 14:48:28 +0100

    port 8008 for http-server

--- spec/protractor.conf.js
+++ spec/protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  baseUrl: 'http://localhost:3000/app/',
+  baseUrl: 'http://localhost:8008/app/',
   specs: ['e2e/*Spec.js'],
   multiCapabilities: [
     {browserName: 'firefox'},
