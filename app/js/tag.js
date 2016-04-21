8b86225d1e18f78fea5402fbb164095622570de1
Jose Lloret <jose.lloret@ed.ac.uk>
2016-04-21 12:52:31 +0100

    tag directive finished

--- app/js/tag.js
+++ app/js/tag.js
@@ -7,14 +7,16 @@
 	 			restrict: 'E',
 	 			templateUrl: 'partials/tag.html',
 	 			bindToController: true,
-	 			scope: {tags: '=tags',selectedTags:'=selectedTags'},
+	 			scope: {tags: '<tags',selectedTags:'<selectedTags',onAdd: '&onAdd'},
 	 			controller: [function(){
 	 				var self = this;
+	 				this.MIN_CHARACTER = 1;
+	 				this.MAX_CHARACTER = 20;
 	 				this.selected = undefined;
-	 				$log.debug('tagCtrl: %o',self);
 	 				this.insertTag = function(tag){
 	 					this.selectedTags.push(tag);
 	 					$log.debug('insertTag: %o',tag);
+	 					if(this.onAdd !== undefined) this.onAdd({tag:tag});
 	 				};
 	 				this.deleteTag = function(id){
 	 					$log.debug('deleteTag with id: %o',id);
@@ -34,12 +36,32 @@
 	 						}
 	 					);
 	 				};
-	 				this.addSuggestion = function(suggestion){
-
+	 				this.onKeyUp = function($event){
+	 					if($event.key === 'Enter'){
+	 						if(this.selected !== undefined && this.selected.length >= this.MIN_CHARACTER && this.selected.length <= this.MAX_CHARACTER){
+	 							$log.debug('onKeyUp: %o',this.selected);
+	 							this.insertTag({id:Date.now(),text:this.selected});
+	 							this.selected = undefined;
+	 						}
+	 					}
 	 				};
-	 				this.onSelect = function($item){	//$item,$model,$label,$event
-	 					this.insertTag({id:Date.now(),text:$item});
-	 					this.selected = undefined;
+	 				this.onSelect = function($item,$model,$label,$event){	//$item,$model,$label,$event
+	 					if($event.type === 'click'){
+	 						if(this.selected !== undefined && this.selected.length >= this.MIN_CHARACTER && this.selected.length <= this.MAX_CHARACTER){
+	 							$log.debug('onSelect: %o',$item);
+	 							this.insertTag({id:Date.now(),text:$item});
+	 							this.selected = undefined;
+	 						}
+	 					}
+	 				};
+	 				this.$onInit = function(){
+	 					$log.debug('tagCtrl: %o',self);
+	 					if(!Array.isArray(this.tags))
+	 						throw new Error('An array was expected for tags property');
+	 					if(!Array.isArray(this.selectedTags))
+	 						throw new Error('An array was expected for selectedTags property');
+	 					if(this.onAdd !== undefined && typeof this.onAdd !== 'function')
+	 						throw new Error('A function was expected for onAdd property');
 	 				};
 	 			}],
 	 			controllerAs: 'tagCtrl'
@@ -51,17 +73,11 @@
 	'use strict';
 	var angular = window.angular;
 	angular.module('app')
-		.controller('tagParentCtrl',[function(){
-			this.tags = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-							  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-							  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-							  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-							  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-							  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-							  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-							  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-							  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-			];
-			this.selectedTags = [{id:1,text:'hello World'},{id:2,text:'bye World'}];
+		.controller('tagParentCtrl',['$log',function($log){
+			this.tags = ['A','B','C'];
+			this.selectedTags = [];
+			this.onAdd = function(tag){
+				$log.debug('onAdd event fired with %o',tag);
+			};
 		}]);
 })(this.window);
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 (function(){
 	'use strict';
 	var gulp = require('gulp');
-	var runSequence = require('run-sequence'); 
+	var fs = require('fs');
+	var runSequence = require('run-sequence');
 
+	function readPackageFile(successCb,errorCb){
+		if(typeof successCb !== 'function' || typeof errorCb !== 'function') return ;
+		fs.readFile('package.json','utf8',function(err, data) {
+			if(err) errorCb('package.json cannot be read');
+			else successCb(JSON.parse(data));
+		});
+	}
 	gulp.task('html2js',function(){
 		var html2js = require('gulp-html2js');
 		return gulp.src('app/partials/*.html')
@@ -11,27 +19,54 @@
 					base: 'app/',
 					name: 'partials'
 				}))
-				.pipe(gulp.dest('app/js/'));
+				.pipe(gulp.dest('dist'));
 	});
 	gulp.task('concat',function(){
 		var concat = require('gulp-concat');
-		return gulp.src(['app/index.js','app/js/*.js'])
+		return gulp.src(['app/index.js','app/js/*.js','dist/partials.js'])
 				.pipe(concat({
 					path: 'index.js'
 				}))
 				.pipe(gulp.dest('dist'));
 	});
 	gulp.task('uglify',function(){
+		var rename = require('gulp-rename');
 		var uglify = require('gulp-uglify');
 		return gulp.src('dist/index.js')
+				.pipe(rename('index.min.js'))
 				.pipe(uglify())
 				.pipe(gulp.dest('dist'));
+	});
+	gulp.task('header',function(){
+		var header = require('gulp-header');
+		readPackageFile(
+			function(data){
+				var banner = ['/**',
+					'* @licence: '+data.name+' v'+data.version,
+					'* (c) 2015-'+new Date().getFullYear()+' '+data.author.name+' - '+data.author.email,
+					'* licence: '+data.license,
+					'*/',
+					''
+				].join('\n');
+				gulp.src('dist/*.js')
+					.pipe(header(banner))
+					.pipe(gulp.dest('dist'));
+			},
+			function(error){
+				console.log(error);
+			}
+		);
 	});
 	gulp.task('processHtml',function(){
 		var processhtml = require('gulp-processhtml');
 		return gulp.src('app/index.html')
 				.pipe(processhtml({}))
 				.pipe(gulp.dest('dist'));
+	});
+	gulp.task('clean',function(){
+		var clean = require('gulp-clean');
+		return gulp.src('dist/partials.js', {read:false})
+				.pipe(clean());
 	});
 	gulp.task('unit-test',function(done){
 		var Server = require('karma').Server;
@@ -40,12 +75,15 @@
 			singleRun: true
 		},done).start();
 	});
+	gulp.task('unit-test-phantomJS',function(done){
+		var Server = require('karma').Server;
+		new Server({
+			configFile: __dirname + '/spec/unit/unit.conf.js',
+			singleRun: true,
+			browsers: ['PhantomJS']
+		},done).start();
+	});
 	gulp.task('build',function(cb){
-		runSequence('html2js','concat',['uglify','processHtml'],cb);
+		runSequence('html2js','concat','uglify',['header','clean','processHtml'],cb);
 	});
 })();
-/*
-TODO
-	-Change name of file after uglify to name.min.js
-	-Load template directly on the unit test
-*/
